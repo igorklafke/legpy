fa32e0f010f8638c1272631937d9bc545652ac0b
Igor Klafke <igor.klafke@gmail.com>
2014-12-08 22:36:02 -0200

    Telas de login e de busca

--- legpy.py
+++ legpy.py
@@ -1,4 +1,5 @@
 # -*- coding: cp860 -*-
+from PyQt4.QtGui import *
 import sys, getopt
 import requests
 import rarfile
@@ -7,14 +8,29 @@ import re
 import os
 import operator
 from HTMLParser import HTMLParser
+from easysettings import EasySettings
 
 base_url = "http://legendas.tv"
+
+settings = EasySettings("settings.conf")
 
 def substitui_caracteres(texto):
     return re.sub('[^0-9a-zA-Z]+', '%20', texto).upper()
 
 class LegPy():
-    def __init__(self, path):
+    def login(self, usuario, senha):
+        s = requests.Session()
+        url = "%s/login" % base_url
+        payload = {'data[User][username]':usuario, 'data[User][password]': senha, "data[lembrar]": "on"}
+        r = s.post(url, payload)
+        html = r.content
+
+        if "<title>Login - Legendas TV</title>" in html:
+            return 0
+        else:
+            return 1
+
+    def inicia_busca(self, path):
         arquivos = os.listdir(path)
 
         for arq in arquivos:
@@ -35,8 +51,10 @@ class LegPy():
             if not os.path.exists(path_srt):
                 print "buscando legenda para %s" % nome_sem_ext
                 nome_pesquisa = substitui_caracteres(nome_sem_ext)
+
                 url = "%s/legenda/busca/%s/1" % (base_url, nome_pesquisa)
                 r = requests.get(url)
+
                 html = r.text
                 parser = self.PesquisaParser(path, nome_sem_ext)
                 parser.feed(html)
@@ -56,13 +74,18 @@ class LegPy():
             i = 0
             while contem_termo and i < tam:
                 contem_termo = termos_array[i] in texto.upper()
+                if not contem_termo and operator.eq(termos_array[i].upper(), 'BLURAY'):
+                    contem_termo = 'BRRIP' in texto.upper()
                 i += 1
             return contem_termo
 
         def nome_arquivo_igual(self, nome):
             nome = nome[:nome.rindex('.')]
             nome = substitui_caracteres(nome)
-            return operator.eq(nome, self.termo_pesquisa)
+            igual = operator.eq(nome, self.termo_pesquisa)
+            if not igual and 'BLURAY' in self.termo_pesquisa:
+                igual = operator.eq(nome, self.termo_pesquisa.replace('BLURAY', 'BRRIP'))
+            return igual
 
         def extrair(self, ziprar):
             extensao = 'srt'
@@ -72,6 +95,7 @@ class LegPy():
             baixou = 0
             for f in ziprar.infolist():
                 if self.nome_arquivo_igual(f.filename):
+                    print "nome arquivo igual"
                     with open(destino, "wb") as out_srt:
                         out_srt.write(ziprar.read(f))
                         baixou = 1
@@ -93,7 +117,15 @@ class LegPy():
                         link_download = '%s/downloadarquivo/%s' % (base_url, id_arquivo)
                         print "legenda encontrada"
                         print "baixando a partir de %s" % link_download
-                        req = requests.get(link_download)
+
+                        s = requests.Session()
+                        settings = EasySettings("settings.conf")
+                        
+                        url = "%s/login" % base_url
+                        payload = {'data[User][username]':settings.get("u"), 'data[User][password]': settings.get("p"), "data[lembrar]": "on"}
+                        s.post(url, payload)
+
+                        req = s.get(link_download)
                         rar_bytes = req.content
                         temp_rar = '%s\%s' % (self.path_arquivos, "temp.rar")
                         with open(temp_rar, "wb") as out_file:
@@ -108,8 +140,99 @@ class LegPy():
                             self.extrair(rf)
                         os.remove(temp_rar)
 
-args = sys.argv[1:]
-if len(args) > 0:
-    path_arquivos = args[0]
-    if len(path_arquivos) > 0:
-        LegPy(path_arquivos)
+class SearchDialog(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setWindowTitle("Buscar Legendas")
+
+        layout = QGridLayout()
+
+        path_label = QLabel(u"Caminho v¡deos")
+        self.path_edit = QLineEdit()
+
+        button = QPushButton("Buscar")
+
+        layout.addWidget(path_label, 0, 0)
+        layout.addWidget(self.path_edit, 0, 1)
+        layout.addWidget(button, 1, 1)
+
+        path_salvo = settings.get("path")
+        if path_salvo:
+            self.path_edit.setText(path_salvo)
+
+        self.setLayout(layout)
+
+        button.clicked.connect(self.click_busca)
+
+    def click_busca(self):
+        path = self.path_edit.text()
+        if path:
+            settings.set("path", path)
+            settings.save()
+            lp.inicia_busca(path)
+            self.close()
+
+class LoginDialog(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setWindowTitle("Login Legendas.TV")
+        layout = QGridLayout()
+        usuario_label = QLabel(u"Usu rio")
+        senha_label = QLabel("Senha")
+
+        self.status_label = QLabel("")
+
+        self.usuario_edit = QLineEdit()
+        self.senha_edit = QLineEdit()
+        self.senha_edit.setEchoMode(QLineEdit.Password)
+
+        button = QPushButton("Login")
+
+        layout.addWidget(usuario_label, 0, 0)
+        layout.addWidget(self.usuario_edit, 0, 1)
+        layout.addWidget(senha_label, 1, 0)
+        layout.addWidget(self.senha_edit, 1, 1)
+
+        layout.addWidget(self.status_label)
+
+        layout.addWidget(button, 3, 1)
+        
+        self.setLayout(layout)
+
+        button.clicked.connect(self.login)
+
+    def login(self):
+        usuario = "%s" % self.usuario_edit.text()
+        senha = "%s" % self.senha_edit.text()
+        if lp.login(usuario, senha):
+            print "logou pelo form"
+            settings.set("u", usuario)
+            settings.set("p", senha)
+            settings.save()
+            self.close()
+            searchDialog.show()
+        else:
+            print "erro no login pelo form"
+            self.close()         
+        
+
+app = QApplication(sys.argv)
+
+lp = LegPy()
+searchDialog = SearchDialog()
+
+usuario = settings.get("u")
+senha = settings.get("p")
+logou = 0
+
+if usuario and senha:
+    if lp.login(usuario, senha):
+        logou = 1
+
+if logou:
+    searchDialog.show()
+else:
+    loginDialog = LoginDialog()
+    loginDialog.show()
+
+app.exec_()
